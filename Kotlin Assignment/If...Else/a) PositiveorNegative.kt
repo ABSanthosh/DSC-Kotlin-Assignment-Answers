25af4492120a136fdffc8e209ac9a362cbe686e7
Santhosh <a.b.santhosh02@gmail.com>
2020-12-14 20:12:39 +0530

    Answers to Assignment uploaded

---
+++ Kotlin Assignment/If...Else/a) PositiveorNegative.kt
@@ -0,0 +1,8 @@
+// check if the given number is Positive or negative
+
+fun main() {
+    val n:Int = 0
+    if(n<0)println("$n is a negative number")
+    else if(n>0)println("$n is a positive number")
+    else println("$n is neither positive nor negative")
+}
